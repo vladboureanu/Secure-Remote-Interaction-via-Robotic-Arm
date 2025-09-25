0967abbc9bce5d2d45b4b879a0f0281b386dd6d4
vladboureanu <79513654+vladboureanu@users.noreply.github.com>
2025-09-25 21:30:18 +0100

    Update clamp_close.py

--- clamp_close.py
+++ clamp_close.py
@@ -4,7 +4,7 @@ import serial, time
 ser = serial.Serial("/dev/ttyUSB0", 115200, timeout=1)
 time.sleep(0.2)
 
-# Close clamp (servo 1 → position 700)
+# Close clamp
 ser.write(b"servo 1 700 400\r")
 ser.close()
 
